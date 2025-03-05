c080b4555fa0585e577daae96d74db4b0deabe91
MohammedSIssa <mohammedsissa1999@gmail.com>
2025-03-05 14:46:09 +0200

    New Dev Update

--- src/model/data.js
+++ src/model/data.js
@@ -877,7 +877,7 @@ export const myData = {
               
               <img src="https://i.imgur.com/Sn3VjvR.jpeg" class="">
 
-              <img src="https://i.imgur.com/GYOUzdq.jpeg" class="">
+              <img src="https://i.imgur.com/NrjHuUZ.jpeg" class="">
                 
           </div>
         </div>
