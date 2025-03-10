bea58592705f984fb0850ce9769bf1926bc1794a
MohammedSIssa <mohammedsissa1999@gmail.com>
2025-03-11 01:51:14 +0200

    Adjusting Script

--- buildAndUpdate.sh
+++ buildAndUpdate.sh
@@ -1,5 +1,5 @@
-git add src/model/data.js
-git commit -m "New Dev Update"
+git add src/model/weeks.json src/model/goals.js src/model/weeks.full.json
+git commit -m "New Week Update"
 git push origin main
 
 ./deployScript.sh
