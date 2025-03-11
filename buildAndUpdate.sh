facc8be7e7de1af136a4e7c198b43e2088c0dc98
MohammedSIssa <mohammedsissa1999@gmail.com>
2025-03-11 02:03:05 +0200

    Adjusted build script

--- buildAndUpdate.sh
+++ buildAndUpdate.sh
@@ -1,4 +1,6 @@
-git add src/model/weeks.json src/model/goals.js src/model/weeks.full.json
+git add src/model/weeks.json
+git add src/model/goals.js  
+git add src/model/weeks.full.json
 git commit -m "New Week Update"
 git push origin main
 
