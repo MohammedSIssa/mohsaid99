045a3074f9eb9672fd5743880ed10a11b2ff31f7
MohammedSIssa <mohammedsissa1999@gmail.com>
2025-05-30 12:09:11 +0300

    Auto-update: 2025-05-30 12:09:11

--- src/components/GoalSection.jsx
+++ src/components/GoalSection.jsx
@@ -1,7 +1,14 @@
 const GoalSection = ({ goal }) => {
+  const isDone = goal.isDone != null && Boolean(goal?.isDone);
+  const isFail = goal.isFailed != null && Boolean(goal?.isFailed);
   return (
-    <div className="text-lg sm:text-xl md:text-2xl lg:text-3xl mb-3 md:mb-5">{goal.name}</div>
-  )
-}
+    <div
+      className="text-lg sm:text-xl md:text-2xl lg:text-3xl mb-3 md:mb-5"
+      style={{ color: isDone ? "var(--finsied-goal-color)" : isFail ? 'var(--failed-goal-color)' : 'var(--font-color)' }}
+    >
+      {goal.name}
+    </div>
+  );
+};
 
-export default GoalSection
+export default GoalSection;
--- src/components/WeekList.jsx
+++ src/components/WeekList.jsx
@@ -1,10 +1,18 @@
 export default function WeekList({ list }) {
   return (
-    <ul className="week-list">
+    <ul className="week-list text-center">
       {list.map((listItem, index) => {
         return (
           <li key={index} className="mb-5">
-            <a href={`${listItem.link === "#" ? "https://www.instagram.com/mohsaid.99/" : listItem.link}`} target="_blank" className="text-lg px-5 max-w-[700px] text-center md:text-xl font-bold">
+            <a
+              href={`${
+                listItem.link === "#"
+                  ? "https://www.instagram.com/mohsaid.99/"
+                  : listItem.link
+              }`}
+              target="_blank"
+              className="text-lg px-5 max-w-[700px] text-center md:text-xl font-bold"
+            >
               {listItem.title}
             </a>
           </li>
