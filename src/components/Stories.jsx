492b41b8657f5a4b12bc3dc88b591f42c9c4ab8d
MohammedSIssa <mohammedsissa1999@gmail.com>
2025-06-04 15:03:58 +0300

    Auto-update: 2025-06-04 15:03:58

--- src/components/Stories.jsx
+++ src/components/Stories.jsx
@@ -1,33 +1,3 @@
-// import { dataCenter } from "../model/center";
-// import Story from "./Story";
-
-// const Stories = ({ type }) => {
-//   const { weeks, goals } = dataCenter;
-//   return (
-//     <div className="stories mt-8 md:mt-25 border-0 border-b-1 h-47 flex gap-8 items-center p-5 py-10 overflow-x-auto scroll-snap-x snap-mandatory scroll-smooth w-full">
-//       {type === "weeks" ? (
-//         weeks.map((week, i) => (
-//           <Story
-//             type={type}
-//             data={week.meta}
-//             id={week.id}
-//             key={i}
-//             isSpecial={week?.isSpecial}
-//           />
-//         ))
-//       ) : type === "goals" ? (
-//         goals.map((goal, i) => (
-//           <Story type={type} data={goal.meta} id={goal.id} key={i} />
-//         ))
-//       ) : (
-//         <h1 className="w-full text-center text-2xl">تأكد من عنوان الرابط!</h1>
-//       )}
-//     </div>
-//   );
-// };
-
-// export default Stories;
-
 import { useRef, useState, useEffect } from "react";
 import { dataCenter } from "../model/center";
 import Story from "./Story";
@@ -46,8 +16,8 @@ const Stories = ({ type }) => {
     const scrollLeft = Math.abs(el.scrollLeft);
     const maxScrollLeft = el.scrollWidth - el.clientWidth;
 
-    setAtStart(scrollLeft >= maxScrollLeft - 1); // اقصى اليسار → بداية القائمة (لأن rtl)
-    setAtEnd(scrollLeft <= 1); // اقصى اليمين → نهاية القائمة
+    setAtStart(scrollLeft >= maxScrollLeft - 1);
+    setAtEnd(scrollLeft <= 1);
   };
 
   useEffect(() => {
@@ -60,9 +30,6 @@ const Stories = ({ type }) => {
     return () => el.removeEventListener("scroll", handleScroll);
   }, []);
 
-  // في rtl:
-  // الزر اليمين: scroll content left (نقل المحتوى لليسار)
-  // الزر اليسار: scroll content right (نقل المحتوى لليمين)
   const scrollLeft = () => {
     storiesRef.current?.scrollBy({
       left: storiesRef.current.clientWidth,
