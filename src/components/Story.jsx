298859f9448218fc162b0e1bd4af57d53868d349
MohammedSIssa <mohammedsissa1999@gmail.com>
2025-06-26 10:40:33 +0300

    Auto-update: 2025-06-26 10:40:33

--- src/components/Story.jsx
+++ src/components/Story.jsx
@@ -5,7 +5,7 @@ export default function Story({ data, id, type, isSpecial }) {
   const defaultClassName = `w-[240px] min-w-[240px] h-full border-1 rounded-lg p-3 px-4 relative snap-start ${
     goldStory ? "" : ""
   }`;
-  const activeClassName = `active brightness-140 ${
+  const activeClassName = `active brightness-110 ${
     goldStory ? "shadow-yellow-900 shadow-xl" : "shadow-slate-800 shadow-xl"
   }`;
   return (
@@ -32,7 +32,7 @@ export default function Story({ data, id, type, isSpecial }) {
       <p className="italic absolute bottom-1 left-3 story-count font-bold">
         #{id}
       </p>
-      <p className={`italic absolute top-[-21px] left-3 story-year skew-x-[-2deg] text-zinc-50 font-bold p-[1px] px-[10px] rounded-md ${goldStory ? "bg-yellow-900" : "bg-rose-700"}`}>
+      <p className={`italic absolute top-[-21px] left-3 story-year skew-x-[-2deg] font-bold p-[1px] px-[10px] rounded-md ${goldStory ? "bg-yellow-400 text-yellow-900" : "bg-rose-700 text-zinc-50"}`}>
         {data.year}
       </p>
     </NavLink>
