1bd5c26e467591810b036a79be74f59e701f2e00
MohammedSIssa <mohammedsissa1999@gmail.com>
2025-11-02 18:33:53 +0200

    Auto-update: 2025-11-02 18:33:53

--- src/pages/Admin/Logs.jsx
+++ src/pages/Admin/Logs.jsx
@@ -1,9 +1,46 @@
-import React from 'react'
+import { API } from "../../scripts/globals";
+import { useState, useEffect } from "react";
+
+import { fetchWithCache } from "../../scripts/cache";
 
 const Logs = () => {
-  return (
-    <div>Logs page</div>
-  )
-}
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
-export default Logs
+  useEffect(() => {
+    async function getLogs() {
+      try {
+        setLoading(true);
+        const res = await fetchWithCache(API + "/logs");
+        setData(res);
+      } catch (err) {
+        console.error(err);
+        setError(err);
+      } finally {
+        setLoading(false);
+      }
+    }
+
+    getLogs();
+  }, []);
+
+  if (loading) return <h1>Loading logs</h1>;
+  if (error) return <h1>Error getting logs</h1>;
+
+  if (data) {
+    return (
+      <div className="flex flex-col gap-5">
+        {data.map((log, idx) => (
+          <div className="flex gap-5" key={idx}>
+						<p>{log.user}</p>
+						<p>{log.details}</p>
+						<p>{log.visited}</p>
+					</div>
+        ))}
+      </div>
+    );
+  }
+};
+
+export default Logs;
