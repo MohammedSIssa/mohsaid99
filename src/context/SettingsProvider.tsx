dae3cb0fec312c8822e10ecde0ad7f8dfd63a9bc
MohammedSIssa <mohammedsissa1999@gmail.com>
2026-01-07 09:40:41 +0200

    Auto-update: 2026-01-07 09:40:41

--- src/context/SettingsProvider.tsx
+++ src/context/SettingsProvider.tsx
@@ -1,13 +1,14 @@
 import { useState } from "react";
 import { SettingsContext } from "./SettingsContext";
 import type { Setting } from "../types/Setting";
+import { loadSettings } from "../variables/localStorage";
 
 export default function SettingsProvider({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const [settings, setSettings] = useState<Setting | null>(null);
+  const [settings, setSettings] = useState<Setting | null>(loadSettings);
   return (
     <SettingsContext.Provider value={{ settings, setSettings }}>
       {children}
