0794509de866a1bf0285f46b5aac2e05a241c61b
mohamed <mohammedsissa1999@gmail.com>
2026-02-23 13:56:24 +0200

    Auto-update: 2026-02-23 13:56:24

--- src/variables/authStorage.ts
+++ src/variables/authStorage.ts
@@ -1,7 +1,10 @@
 export const getToken = () =>
   JSON.parse(localStorage.getItem("token") ?? "null");
 
-export const setTokenLocal = (token: string) =>
-  localStorage.setItem("token", JSON.stringify(token));
+export const setTokenLocal = (token: string | null | undefined) => {
+  if (token != null) {
+    localStorage.setItem("token", JSON.stringify(token));
+  }
+};
 
 export const removeToken = () => localStorage.removeItem("token");
