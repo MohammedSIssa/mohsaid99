0885784119bbc064bb1cf0204f6167f42dfb3c36
mohamed <mohammedsissa1999@gmail.com>
2026-02-23 13:46:34 +0200

    Auto-update: 2026-02-23 13:46:34

--- src/variables/formatData.ts
+++ src/variables/formatData.ts
@@ -9,10 +9,30 @@ export function formatTimeAgo(dateString: string) {
   if (diffInSeconds < 60) return `قبل ${diffInSeconds} ثانية`;
 
   const minutes = Math.floor(diffInSeconds / 60);
+  if (minutes === 1) {
+    return "قبل دقيقة";
+  }
+  if (minutes === 2) {
+    return "قبل دقيقتين";
+  }
+  if (minutes >= 3 && minutes > 11) {
+    return `قبل ${minutes} دقائق`;
+  }
   if (minutes < 60) return `قبل ${minutes} دقيقة`;
 
   const hours = Math.floor(minutes / 60);
-  if (hours < 24) return `قبل ${hours} ساعة`;
+  if (hours === 1) {
+    return "قبل ساعة";
+  }
+  if (hours === 2) {
+    return "قبل ساعتين";
+  }
+  if (hours >= 3 && hours < 11) {
+    return `قبل ${hours} ساعات`;
+  }
+  if (hours < 24) {
+    return `قبل ${hours} ساعة`;
+  }
 
   return date.toLocaleString("ar-SA", {
     month: "short",
