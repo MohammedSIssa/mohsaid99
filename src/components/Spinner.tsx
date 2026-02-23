4fe212b3fe8330a0d1b5de7302aab7380dd2dad8
mohamed <mohammedsissa1999@gmail.com>
2026-02-23 14:34:19 +0200

    Auto-update: 2026-02-23 14:34:19

--- src/components/Spinner.tsx
+++ src/components/Spinner.tsx
@@ -2,8 +2,8 @@ import Spinner from "../assets/icons/spinner.svg";
 
 export default function SpinLoader() {
   return (
-    <div className="w-7 h-7 spin">
-      <img src={Spinner} width={28} height={28} />
+    <div className="spin-parent">
+      <img src={Spinner} className="spin" width={28} height={28} />
     </div>
   );
 }
