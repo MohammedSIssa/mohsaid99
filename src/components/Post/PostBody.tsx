017c46c5ba4175db39a0492abe965dd0d4d81fa3
mohamed <mohammedsissa1999@gmail.com>
2026-02-27 15:56:06 +0200

    Auto-update: 2026-02-27 15:56:05

--- src/components/Post/PostBody.tsx
+++ src/components/Post/PostBody.tsx
@@ -1,38 +1,31 @@
-import React, { lazy, Suspense } from "react";
-// import { miniMarkdownToHTML } from "../../variables/miniMD";
+// import React, { lazy, Suspense } from "react";
 import Marked from "../Marked";
 
-const MarkdownContent = lazy(() => import("../MarkdownContent"));
+// const MarkdownContent = lazy(() => import("../MarkdownContent"));
 
-class ErrorBoundary extends React.Component<
-  { children: React.ReactNode; fallback: React.ReactNode },
-  { hasError: boolean }
-> {
-  state = { hasError: false };
+// class ErrorBoundary extends React.Component<
+//   { children: React.ReactNode; fallback: React.ReactNode },
+//   { hasError: boolean }
+// > {
+//   state = { hasError: false };
 
-  static getDerivedStateFromError() {
-    return { hasError: true };
-  }
+//   static getDerivedStateFromError() {
+//     return { hasError: true };
+//   }
 
-  render() {
-    if (this.state.hasError) {
-      return this.props.fallback;
-    }
+//   render() {
+//     if (this.state.hasError) {
+//       return this.props.fallback;
+//     }
 
-    return this.props.children;
-  }
-}
+//     return this.props.children;
+//   }
+// }
 
 export default function PostBody({ body, dir }: { body: string; dir: string }) {
-  // const fallbackHTML = miniMarkdownToHTML(body);
-
   return (
     <div className={`not-prose block w-full py-2 ${dir}`}>
-      <ErrorBoundary fallback={<Marked content={body} dir={dir} />}>
-        <Suspense fallback={<Marked content={body} dir={dir} />}>
-          <MarkdownContent dir={dir} content={body} />
-        </Suspense>
-      </ErrorBoundary>
+      <Marked content={body} dir={dir} />
     </div>
   );
 }
