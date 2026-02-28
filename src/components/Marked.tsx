7714001bd8ff0bd659b7870dd343749c5145e9c2
mohamed <mohammedsissa1999@gmail.com>
2026-02-28 10:52:51 +0200

    Auto-update: 2026-02-28 10:52:51

--- src/components/Marked.tsx
+++ src/components/Marked.tsx
@@ -1,7 +1,18 @@
 import { marked } from "marked";
+import { useEffect, useState } from "react";
 
 const Marked = ({ content, dir }: { content: string; dir: string }) => {
-  const html = marked.parse(content);
+  const [html, setHtml] = useState("");
+
+  useEffect(() => {
+    const parseMarkdown = async () => {
+      const parsedHtml = await marked.parse(content);
+      const processUnChecked = parsedHtml.replace(/<td>(\s*⛔\s*)<\/td>/g, `<td class="undone">$1</td>`);
+      const processChecked = processUnChecked.replace(/<td>(\s*✅\s*)<\/td>/g, `<td class="done">$1</td>`);
+      setHtml(processChecked);
+    };
+    parseMarkdown();
+  }, [content]);
 
   return (
     <div
