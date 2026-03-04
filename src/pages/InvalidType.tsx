9010557fc63a7e89401f8eed0eb22cf7eb345e2d
mohamed <mohammedsissa1999@gmail.com>
2026-03-04 14:43:25 +0200

    Auto-update: 2026-03-04 14:43:25

--- src/pages/InvalidType.tsx
+++ src/pages/InvalidType.tsx
@@ -1,8 +1,12 @@
+import Warning from "../assets/icons/warning.svg";
+
 export default function InvalidType() {
   return (
     <div className="min-h-screen flex items-center justify-center bg-(--bg-color) px-4">
       <div className="bg-(--darker-bg-color) shadow-lg rounded-2xl p-8 text-center max-w-sm w-full">
-        <div className="text-5xl mb-4">⚠️</div>
+        <div className="text-5xl mb-4 flex items-center justify-center">
+          <img src={Warning} width={70} height={70} />
+        </div>
         <h1 className="text-2xl font-bold mb-2">نوع غير صالح</h1>
         <p className=" text-sm">
           عذرًا، النوع الذي تحاول الوصول إليه غير مدعوم أو غير موجود.
