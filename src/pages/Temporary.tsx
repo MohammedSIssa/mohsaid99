173f1099fc8663794e6cf9df312be14e2b76ddbb
mohamed <mohammedsissa1999@gmail.com>
2026-03-24 16:13:12 +0200

    Auto-update: 2026-03-24 16:13:12

--- src/pages/Temporary.tsx
+++ src/pages/Temporary.tsx
@@ -1,4 +1,5 @@
-import Repair from "../assets/icons/repair.svg";
+// import Repair from "../assets/icons/repair.svg";
+import E404 from '../assets/404.png';
 import { logger } from "../variables/logger";
 import { useEffect } from "react";
 
@@ -11,8 +12,8 @@ const Temporary = () => {
   }, []);
   return (
     <div className="flex flex-col md:pr-16 min-h-screen items-center justify-center py-25">
-      <div className="bg-(--darker-bg-color) p-6 w-full md:w-fit border-2 rounded-lg border-(--border-color) flex flex-col items-center">
-        <img src={Repair} width={50} className="mb-10" />
+      <div className="bg-(--darker-bg-color) border-l-2 border-l-fuchsia-400 border-r-2 border-r-blue-500 p-6 w-full md:w-fit rounded-lg flex flex-col items-center">
+        <img src={E404} width={380} className="mb-10 bg-blend-screen rounded-lg brightness-85" />
         <h1 className="font-bold text-2xl">هذا الجزء فيه إصلاحات حالياً</h1>
       </div>
     </div>
